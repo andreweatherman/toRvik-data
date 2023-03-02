a48701ab30e5ef99753efe255fdada5324557daf
andreweatherman <taw38@duke.edu>
2023-03-02 17:20:12 +0000

    [VM PUSH] update team shooting stats

--- update_scripts/update_game_factors.R
+++ update_scripts/update_game_factors.R
@@ -2,39 +2,38 @@ library(tidyverse)
 library(janitor)
 library(rvest)
 library(withr)
-library(arrow)
 
 update_game_factors <- function(year = current_season()) {
   suppressWarnings({
-    withr::local_options(HTTPUserAgent='toRvik Package')
-    if (!(is.numeric(year) && nchar(year) == 4 && year >=
-          2008)) {
-      cli::cli_abort("Enter a valid year as a number (YYYY). Data only goes back to 2008!")
-    } else {
-      names <- c(
-        "date", "type", "team", "conf", "opp", "loc", "result", "adj_o", "adj_d", "off_ppp", "off_efg", "off_to", "off_or", "off_ftr", "def_ppp",
-        "def_efg", "def_to", "def_or", "def_ftr", "game_score", "opp_conf", "season", "tempo", "game_id", "coach", "opp_coach", "avg_marg", "opp_avg_marg"
-      )
-      x <- readr::read_csv(paste0("https://barttorvik.com/getgamestats.php?year=", year, "&csv=1"), col_names = FALSE, show_col_types = FALSE) %>% dplyr::select(-c(22, 30:31))
-      colnames(x) <- names
-      x <- x %>%
-        dplyr::mutate(
-          date = lubridate::mdy(date),
-          type = dplyr::case_when(
-            type == 0 ~ "nc",
-            type == 1 ~ "conf",
-            type == 2 ~ "conf_t",
-            type == 3 ~ "post",
-            TRUE ~ "nond1"
-          )
-        ) %>%
-        dplyr::relocate("opp_conf", .after = "opp") %>%
-        dplyr::relocate("avg_marg", .after = "result") %>%
-        dplyr::relocate("opp_avg_marg", .after = "avg_marg") %>%
-        dplyr::arrange(desc(date))
-      return(x)
-    }
-  })}
+  withr::local_options(HTTPUserAgent='toRvik Package')
+  if (!(is.numeric(year) && nchar(year) == 4 && year >=
+    2008)) {
+    cli::cli_abort("Enter a valid year as a number (YYYY). Data only goes back to 2008!")
+  } else {
+    names <- c(
+      "date", "type", "team", "conf", "opp", "loc", "result", "adj_o", "adj_d", "off_ppp", "off_efg", "off_to", "off_or", "off_ftr", "def_ppp",
+      "def_efg", "def_to", "def_or", "def_ftr", "game_score", "opp_conf", "season", "tempo", "game_id", "coach", "opp_coach", "avg_marg", "opp_avg_marg"
+    )
+    x <- readr::read_csv(paste0("https://barttorvik.com/getgamestats.php?year=", year, "&csv=1"), col_names = FALSE, show_col_types = FALSE) %>% dplyr::select(-c(22, 30:31))
+    colnames(x) <- names
+    x <- x %>%
+      dplyr::mutate(
+        date = lubridate::mdy(date),
+        type = dplyr::case_when(
+          type == 0 ~ "nc",
+          type == 1 ~ "conf",
+          type == 2 ~ "conf_t",
+          type == 3 ~ "post",
+          TRUE ~ "nond1"
+        )
+      ) %>%
+      dplyr::relocate("opp_conf", .after = "opp") %>%
+      dplyr::relocate("avg_marg", .after = "result") %>%
+      dplyr::relocate("opp_avg_marg", .after = "avg_marg") %>%
+      dplyr::arrange(desc(date))
+    return(x)
+  }
+})}
 
 # save to object
 factors <- update_game_factors(2023)
@@ -42,27 +41,14 @@ factors <- update_game_factors(2023)
 # set wd
 setwd('~/torvik-data')
 
-# read all factors
-all_factors <- arrow::read_parquet("game_factors/all_factors.parquet")
-
-# combine new data
-all_factors <- dplyr::bind_rows(all_factors, factors)
-
-
 # write to .csv
 write.csv(
   factors,
   file = 'game_factors/game_factors_2023.csv'
 )
 
-# write to .parquet
-arrow::write_parquet(
-  all_factors,
-  sink = 'game_factors/all_factors.parquet'
-)
-
 # push to github
 system('git pull')
-system('git add .')
+system('git add game_factors/game_factors_2023.csv')
 system("git commit -m '[VM PUSH] update game factors'")
 system('git push')
